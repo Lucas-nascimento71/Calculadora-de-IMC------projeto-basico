bda62635fee14ac36f89db104ec96fefcfa9f8de
Lucas-nascimento71 <lnfsouza@hotmail.com>
2024-09-28 00:38:36 -0300

    Adicionei estruturas de condições

--- docs/main.py
+++ docs/main.py
@@ -7,7 +7,21 @@ imc = peso / (altura ** 2)
 print(f'Seu IMC: {imc:.1f}')
 
 # Classificação do IMC
-
-
-
-
+if imc < 16.9:
+    Classificação = 'Muito abaixo do Peso'
+elif 17 <= imc <= 18.4:
+    Classificação = 'Abaixo do Peso.'
+elif 18.5 <= imc <= 24.9:
+    Classificação = 'Peso Normal.'
+elif 25 <= imc <= 29.9:
+    Classificação = 'Sobrepeso/Acima do peso.'
+elif 30 <= imc <= 34.9:
+    Classificação = 'Obesidade grau 1.'
+elif 35 <= imc <= 40:
+    Classificação = 'Obesidade grau 2.'
+elif imc > 40:
+    Classificação = 'Obesidade grau 3 (Obesidade Mórbida).'
+    
+# Saída de dados
+print(f'Seu IMC foi de {imc}.')
+print(f'Sua classificação foi de ')
